27d66631152a9b98c9abbd844877d41abc84d0b6
Simon Siefke <simon.siefke@gmail.com>
2018-04-03 20:14:46 +0200

    add compiled files

---
+++ dist/vue-autocomplete-suggestions.cjs.js
@@ -0,0 +1,56 @@
+/**
+ * vue-autocomplete-suggestions v0.0.1
+ * (c) Simon Siefke <simon.siefke@gmail.com>
+ * https://github.com/SimonSiefke/vue-autocomplete-suggestions
+ * Released under the MIT License.
+ */
+
+'use strict';
+
+var Autocomplete = {
+  render: function render() {
+    var _vm = this;
+
+    var _h = _vm.$createElement;
+
+    var _c = _vm._self._c || _h;
+
+    return _c('section', [_c('input', _vm._g(_vm._b({
+      attrs: {
+        "type": "text"
+      },
+      domProps: {
+        "value": _vm.value
+      }
+    }, 'input', _vm.$attrs, false), _vm.listeners)), _vm._v(" "), _c('div', [_c('div', [_vm._t("suggestions")], 2)])]);
+  },
+  staticRenderFns: [],
+  _scopeId: 'data-v-0e3ab061',
+  name: 'vue-autocomplete',
+  inheritAttrs: false,
+  props: {
+    value: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    listeners: function listeners() {
+      var _this = this;
+
+      return Object.assign({}, this.$listeners, {
+        input: function input(event) {
+          return _this.$emit('input', event.target.value);
+        }
+      });
+    }
+  }
+};
+
+var index = {
+  install: function install(Vue, options) {
+    Vue.component(Autocomplete.name, Autocomplete);
+  }
+};
+
+module.exports = index;
